d470de9a11ec71f9d76c27812492348be5e03e96
rg060507 <g016909o@student.staffs.ac.uk>
2025-11-08 01:45:54 +0000

    Modified the code so that a custom Plugin is now being used to make code more readable. I also added a timer resource that waits 2 seconds before outputting any text to the screen.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
 use bevy::prelude::*;
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 #[derive(Component)]
 struct Person;
 
 #[derive(Component)]
 struct Name(String);
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_name, greet_people).chain()))
-        .run();
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, (update_name, greet_people).chain());
+    }
 }
 
-fn hello_world() {
-    println!("Hello, World!");
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, HelloPlugin))
+        .run();
 }
 
 fn add_people(mut commands: Commands) {
@@ -24,9 +31,11 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Peter Capaldi".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query{
-        println!("Hello, {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished(){
+        for name in &query{
+            println!("Hello, {}!", name.0);
+        }
     }
 }
 
