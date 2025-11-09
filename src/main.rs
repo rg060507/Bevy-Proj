1166fb7120665ee7f2b8bd2cd73e7ddddc306416
rg060507 <g016909o@student.staffs.ac.uk>
2025-11-09 00:01:23 +0000

    Added a simple collision detection system that prints to the console when any collision on the x axis is made.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use bevy::prelude::*;
 
-
 const RECT1_COLOR: Color = Color::srgb(1., 0., 0.);
 const RECT2_COLOR: Color = Color::srgb(0., 0., 1.);
+
+const RECT_SPEED: f32 = 500.;
+
+const RECT_SIZE: f32 = 128.;
 
 #[derive(Component)]
 struct Rect1;
@@ -14,11 +17,10 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, (move_rect1, move_rect2))
+        .add_systems(Update, (check_for_collisions, move_rect1, move_rect2))
         .run();
 }
 
-// Startup system to setup the scene and spawn all relevant entities.
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -29,14 +31,14 @@ fn setup(
     commands.spawn((
         Mesh2d(meshes.add(Rectangle::default())),
         MeshMaterial2d(materials.add(RECT1_COLOR)),
-        Transform::from_translation(Vec3::from((0., 0., 1.))).with_scale(Vec3::splat(128.)),
+        Transform::from_translation(Vec3::from((0., 0., 1.))).with_scale(Vec3::splat(RECT_SIZE)),
         Rect1,
     ));
 
     commands.spawn((
         Mesh2d(meshes.add(Rectangle::default())),
         MeshMaterial2d(materials.add(RECT2_COLOR)),
-        Transform::from_translation(Vec3::from((200., 0., 0.))).with_scale(Vec3::splat(128.)),
+        Transform::from_translation(Vec3::from((200., 0., 0.))).with_scale(Vec3::splat(RECT_SIZE)),
         Rect2,
     ));
 }
@@ -56,7 +58,7 @@ fn move_rect1(
         direction += 1.0;
     }
 
-    transform.translation.x += direction * 500. * time.delta_secs();
+    transform.translation.x += direction * RECT_SPEED * time.delta_secs();
 }
 
 fn move_rect2(
@@ -74,5 +76,21 @@ fn move_rect2(
         direction += 1.0;
     }
 
-    transform.translation.x += direction * 500. * time.delta_secs();
+    transform.translation.x += direction * RECT_SPEED * time.delta_secs();
+}
+
+fn check_for_collisions(
+    rect1: Single<&mut Transform, With<Rect1>>,
+    rect2: Single<&mut Transform, (With<Rect2>, Without<Rect1>)>,) {
+
+    let rect1x = rect1.translation.x;
+    let rect2x = rect2.translation.x;
+    let rectSize = RECT_SIZE / 2.;
+
+    if rect1x + rectSize > rect2x - rectSize && rect1x - rectSize < rect2x + rectSize {
+        println!("collisions detected!");
+    }
+    else if !(rect1x + rectSize > rect2x - rectSize && rect1x - rectSize < rect2x + rectSize) {
+        println!("no collisions detected!");
+    }
 }
